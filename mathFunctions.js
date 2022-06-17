1a8a1a5ad8f6a90b2b78b26ea00c7b04c1de907d
ffff19 <fprogramming@protonmail.com>
2022-06-17 09:04:08 -0300

    Add function operate that takes an operator and 2 numbers and then calls one of the math functions defined earlier

--- mathFunctions.js
+++ mathFunctions.js
@@ -13,3 +13,20 @@ function multiply(m, n) {
 function divide(m, n) {
 	return m / n;
 }
+
+function operate(op, m, n) {
+	if (op == '+') {
+		return add(m, n);
+	} else if (op == '-') {
+		return subtract(m, n);
+	} else if (op == '*') {
+		return multiply(m, n);
+	} else if (op == '/') {
+		return divide(m, n);
+	}
+}
+
+console.log(operate('+', 4, 1));
+console.log(operate('-', 6, 1));
+console.log(operate('*', 5, 1));
+console.log(operate('/', 10, 2));

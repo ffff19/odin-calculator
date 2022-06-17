000e8fe196afa06de054b37052fde8cd7f73acb4
ffff19 <fprogramming@protonmail.com>
2022-06-17 08:59:18 -0300

    Add mathFunctions.js

---
+++ mathFunctions.js
@@ -0,0 +1,15 @@
+function add(m, n) {
+	return m + n;
+}
+
+function subtract(m, n) {
+	return m - n;
+}
+
+function multiply(m, n) {
+	return m * n;
+}
+
+function divide(m, n) {
+	return m / n;
+}

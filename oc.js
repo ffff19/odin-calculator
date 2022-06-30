13b8543b14f12ba5bf8ac4f6cd33557a9a6ad0a6
ffff19 <fprogramming@protonmail.com>
2022-06-30 14:36:15 -0300

    improve display

--- oc.js
+++ oc.js
@@ -1,4 +1,4 @@
-let displayValue = 1;
+let displayValue = '';
 let divide = document.querySelector('.divide');
 let multiply = document.querySelector('.multiply');
 let subtract = document.querySelector('.subtract');
@@ -15,54 +15,86 @@ let two = document.querySelector('.two');
 let three = document.querySelector('.three');
 let zero = document.querySelector('.zero');
 let equal = document.querySelector('.equals');
+let display = document.querySelector('#display');
 
+
+fillDisplay('');
 function clickNumberOrOperator() {
+
 	seven.addEventListener('click', e => {
-		displayValue = 7;
+		clearDisplay();
+		displayValue += '7';
+		fillDisplay(displayValue);
 	});
 
 	eight.addEventListener('click', e => {
-		displayValue = 8;
+		clearDisplay();
+		displayValue += '8';
+		fillDisplay(displayValue);
 	});
 
 	nine.addEventListener('click', e => {
-		displayValue = 9;
+		clearDisplay();
+		displayValue += '9';
+		fillDisplay(displayValue);
 	});
 
 	four.addEventListener('click', e => {
-		displayValue = 4;
+		clearDisplay();
+		displayValue += '4';
+		fillDisplay(displayValue);
 	});
 
 	five.addEventListener('click', e => {
-		displayValue = 5;
+		clearDisplay();
+		displayValue += '5';
+		fillDisplay(displayValue);
 	});
 
 	six.addEventListener('click', e => {
-		displayValue = 6;
+		clearDisplay();
+		displayValue += '6';
+		fillDisplay(displayValue);
 	});
 	one.addEventListener('click', e => {
-		displayValue = 1;
+		clearDisplay();
+		displayValue += '1';
+		fillDisplay(displayValue);
 	});
 
 	two.addEventListener('click', e => {
-		displayValue = 2;
+		clearDisplay();
+		displayValue += '2';
+		fillDisplay(displayValue);
 	});
 
 	three.addEventListener('click', e => {
-		displayValue = 3;
+		clearDisplay();
+		displayValue += '3';
+		fillDisplay(displayValue);
 	});
 
 	zero.addEventListener('click', e => {
-		displayValue = 0;
+		clearDisplay();
+		displayValue += '0';
+		fillDisplay(displayValue);
 	});
 
+}
+
+function fillDisplay(value) {
 	const display = document.querySelector('#display');
 
 	const content = document.createElement('div');
 	content.classList.add('content');
-	content.textContent = displayValue;
+	content.textContent = value;
 
 	display.appendChild(content);
+}
+
+function clearDisplay() {
+	let displayContent = display.children[0];
+	display.removeChild(displayContent);
 }
 
 clickNumberOrOperator();
